4cbfda2b67cbbf8eed221ccc3c65978cb46fd4fa
Benjamin Chen <bgc11@duke.edu>
2018-12-08 14:34:15 -0500

    readTreeHeader of decompress

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -59,12 +59,32 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
-
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+		int bits = in.readBits(BITS_PER_INT);
+		if (bits != HUFF_TREE) {
+			throw new HuffException("illegal header starts with " + bits);
 		}
+		if (bits == -1) {
+			throw new HuffException("illegal header starts with " + bits);
+		}
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root, in, out);
 		out.close();
+	}
+
+	private HuffNode readTreeHeader(BitInputStream in) {
+		// read a single bit
+		int bit = in.readBits(1);
+		if (bit == -1) {
+			throw new HuffException("illegal header starts with " + bit);
+		}
+		if (bit == 0) { // if internal node
+			HuffNode left = readTreeHeader(in); // reads left subtree
+			HuffNode right = readTreeHeader(in); // reads right subtree
+			return new HuffNode(0, 0, left, right);
+		}
+		else { // if leaf node
+			int value = in.readBits(BITS_PER_WORD + 1); // value stored in leaf
+			return new HuffNode(value, 0, null, null);
+		}
 	}
 }
